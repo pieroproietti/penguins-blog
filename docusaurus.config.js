6227c58d77c65644f9424937ac409b354d96dbad
Piero Proietti <piero.proietti@gmail.com>
2023-03-26 19:51:33 +0200

    logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
         title: 'penguins\' eggs',
         logo: {
           alt: 'Penguins Eggs Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
