74e3ad8f70fb8d7593008fae3728b53e73c6a2ea
artisan <artisan@192.168.1.3>
2024-11-15 18:24:18 +0100

    noia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -157,16 +157,3 @@ const config = {
 };
 
 export default config;
-module.exports = {
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        gtag: {
-          trackingID: 'G-DCRVZRVWEV', 
-          anonymizeIP: true,
-        },
-      },
-    ],
-  ],
-};
