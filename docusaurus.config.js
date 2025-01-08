ca5c1c790da501b7cb8a7fe2f2309b68019efac7
Piero Proietti <piero.proietti@gmail.com>
2025-01-08 20:37:19 +0100

    gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,12 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({    
-        docs: {
+        gtag: {
+            trackingID: 'G-DCRVZRVWEV',
+            anonymizeIP: true,
+          },
+
+          docs: {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
